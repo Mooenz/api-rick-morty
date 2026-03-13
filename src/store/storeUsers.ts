2a5f2da0442e4ada221b7ff11dd4c4639bd19ef4
mooenz <joss92821@hotmail.com>
2026-03-12 22:00:19 -0500

    Refactor storeUsers: remove userSelect and setUserSelect, streamline state management

--- src/store/storeUsers.ts
+++ src/store/storeUsers.ts
@@ -2,11 +2,39 @@ import { create } from 'zustand';
 import type { User } from '@/@types/index.ts';
 
 type storeUsers = {
-	userSelect: User | null;
-	setUserSelect: (user: User | null) => void;
+	users: User[];
+	page: string;
+	filterState: string;
+	filterText: string;
+	count: number;
+	pages: number;
+	next: string | null;
+	prev: string | null;
+	setPageInfo: (pageInfo: { count: number; pages: number; next: string | null; prev: string | null }) => void;
+	setFilterState: (setFilterState: string) => void;
+	setFilterText: (filterText: string) => void;
+	setUsers: (users: User[]) => void;
+	setPage: (page: string) => void;
 };
 
 export const useStoreUsers = create<storeUsers>((set) => ({
-	userSelect: null,
-	setUserSelect: (user) => set({ userSelect: user }),
+	users: [],
+	page: '0',
+	filterState: '',
+	filterText: '',
+	count: 0,
+	pages: 0,
+	next: null,
+	prev: null,
+	setPageInfo: (pageInfo) =>
+		set({
+			count: pageInfo.count,
+			pages: pageInfo.pages,
+			next: pageInfo.next,
+			prev: pageInfo.prev,
+		}),
+	setFilterState: (filterState) => set({ filterState }),
+	setFilterText: (filterText) => set({ filterText }),
+	setUsers: (users) => set({ users }),
+	setPage: (page) => set({ page }),
 }));
